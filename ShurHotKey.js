e69f69b89da30760a40d58d11aab3de9f8bb0fec
Carlos Alberto Pérez Marín <carlosalbertomix10@gmail.com>
2015-12-06 22:36:02 +0100

    Added more hotkeys
    alt+:
    *g GENERAL
    *i INFORMATICA
    *f FOTOGRAFIA
    *e EMPLEO
    *t TAXIS
    *v VIAJES
    *q QUEDADAS

--- ShurHotKey.js
+++ ShurHotKey.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ShurHotKey
-// @version      0.1
+// @version      0.2
 // @description  Recorre el foro con el teclado ->
 // @author       Karucida
 // @include      http://www.forocoches.com/foro/*
@@ -11,6 +11,7 @@
 $(document).ready(function() {
     $(document).keydown(function(){
         if($("a[rel='next']" ).text()==">>"||$( "a[rel='prev']" ).text()=="<<"){
+            //39 -> NEXT
         if(event.which==39){
             if($( "a[rel='next']" ).text()==">>"){
                 location.href = $( "a[rel='next']" ).prop( "href" );
@@ -21,6 +22,7 @@ $(document).ready(function() {
                     location.href = $( "a[title^='Mostrar Resultados del 1']" ).prop( "href" );
             }
         }
+            //37 <- BACK
         if(event.which==37){
             if($( "a[rel='prev']" ).text()=="<<"){
                 location.href = $( "a[rel='prev']" ).prop( "href" );
@@ -31,6 +33,39 @@ $(document).ready(function() {
                     location.href = $( "a[title^='Mostrar']:last" ).prop( "href" );
             }
         }
+    }
+
+    if(event.shiftKey){    
+    switch (event.which) {
+        case 71:
+            //71 g  GENERAL
+            location.href ="http://www.forocoches.com/foro/forumdisplay.php?f=2"
+            break;
+        case 73:
+            //73 i INFORMATICA
+            location.href = "http://www.forocoches.com/foro/forumdisplay.php?f=17"
+            break;
+        case 70:
+            //70 f FOTOGRAFIA
+           location.href = "http://www.forocoches.com/foro/forumdisplay.php?f=82"
+           break;
+        case 69:
+            //69 e EMPLEO
+            location.href = "http://www.forocoches.com/foro/forumdisplay.php?f=23"
+            break;
+        case 84:
+            //84 t TAXIS
+            location.href = "http://www.forocoches.com/foro/forumdisplay.php?f=64"
+            break;
+        case 86:
+            //86 v VIAJES
+            location.href ="http://www.forocoches.com/foro/forumdisplay.php?f=27" 
+            break;
+        case 81:
+            //81 q QUEDADAS
+            location.href = "http://www.forocoches.com/foro/forumdisplay.php?f=15"
+            break;
+    }
     }
     });
 });
