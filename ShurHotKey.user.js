6c0504e53c01f07933ac8cc9eacd692d8114f0bb
karucida <carlosalbertomix10@gmail.com>
2016-02-01 14:45:51 +0100

    Changed metadata

--- ShurHotKey.user.js
+++ ShurHotKey.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         ShurHotKey
-// @version      0.4
-// @description  Recorre el foro con el teclado ->
+// @version      0.5
+// @description  navegación rápida entre subforos y post con más de una página
 // @author       Karucida
 // @include      http://www.forocoches.com/foro/*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js
 // @grant        none
-// @downloadURL    https://github.com/Karucida/ShurHotKey/raw/master/ShurHotKey.user.js
-// @updateURL    https://github.com/Karucida/ShurHotKey/raw/master/ShurHotKey.user.js
+// @downloadURL    https://raw.githubusercontent.com/Karucida/ShurHotKey/master/ShurHotKey.min.user.js
+// @updateURL    https://raw.githubusercontent.com/Karucida/ShurHotKey/master/ShurHotKey.min.user.js
 // ==/UserScript==
 
 $(document).ready(function () {
